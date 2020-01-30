780388b63dde6e70d0f75493da98029b63382d76
Sam Hammoud <sam@genio.io>
2020-01-30 13:29:41 +0000

    Add Parallels Add Intranet Query - Group Emails.sql

---
+++ Intranet Queries/Group Emails.sql
@@ -0,0 +1,11 @@
+SELECT CM.slug                              AS "Artist",
+       GROUP_CONCAT(U.email SEPARATOR ', ') AS "Team",
+       CM.created_at AS "Created at",
+       CM.updated_at AS "Created at",
+       CM.dormant_date AS "Dormant at"
+
+
+FROM users U
+     LEFT JOIN clients_music_users CMU ON CMU.users_id = U.id
+     LEFT JOIN clients_music CM ON CM.id = CMU.clients_music_id
+GROUP BY CMU.clients_music_id
