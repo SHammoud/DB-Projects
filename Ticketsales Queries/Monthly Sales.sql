87fa3376f481107e39d4b6ccbd95324964a969aa
Sam Hammoud <sam@genio.io>
2020-02-11 14:14:24 +0000

    Add Paradigm Analysis Add Ticketsales Monthly Sales.sql

---
+++ Ticketsales Queries/Monthly Sales.sql
@@ -0,0 +1,23 @@
+SELECT P.pname                            AS 'Artist',
+       S.svenue                           AS 'Venue',
+       FROM_UNIXTIME(S.sdate, '%Y-%m-%d') AS 'Show Date',
+       CASE
+           WHEN
+               MAX(D.datavalue) = MIN(D.datavalue) THEN ABS(MAX(D.datavalue))
+           ELSE
+               ABS(MAX(D.datavalue) - MIN(D.datavalue))
+           END                            AS 'Sold'
+
+
+FROM data_sale_dates DSD
+
+         LEFT JOIN data D on D.sale_date_id = DSD.id
+         LEFT JOIN shows S on S.sid = D.show_id
+         LEFT JOIN performers P on S.pid = P.pid
+         LEFT JOIN tours T on DSD.tour_id = T.tid
+
+# TODO UPDATE SO QUERY CAN BE EXECUTED FOR THIS MONTH OR LAST MONTH
+# You need to add UNIX times here:
+WHERE DSD.time BETWEEN 1577836800 AND 1580428800
+GROUP BY S.sid
+ORDER BY P.pname
