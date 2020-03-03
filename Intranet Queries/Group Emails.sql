39ca5102498d213e6fb6af56815279e8701737f2
Sam Hammoud <sam@genio.io>
2020-03-03 17:25:43 +0000

    Update Group Emails,sql

--- Intranet Queries/Group Emails.sql
+++ Intranet Queries/Group Emails.sql
@@ -1,15 +1,20 @@
 SELECT
        CM.id AS 'ID',
        REPLACE(CM.slug, '-', '') AS "Artist",
-       CHAR_LENGTH(REPLACE(CM.slug, '-', '')) AS "Count",
+       CHAR_LENGTH(REPLACE(CM.slug, '-', '')) AS "Slug Count",
        GROUP_CONCAT(U.email SEPARATOR ', ') AS "Team",
        CM.created_at AS "Created",
        CM.updated_at AS "Updated",
-       CM.inactive_date AS "Inactive"
+       CM.inactive_date AS "Inactive",
+       CM.deleted_at AS "Deleted"
 
 
 FROM users U
      LEFT JOIN clients_music_users CMU ON CMU.users_id = U.id
      LEFT JOIN clients_music CM ON CM.id = CMU.clients_music_id
-WHERE CM.id IS NOT NULL
+WHERE
+      CM.id IS NOT NULL
+AND  CM.deleted_at IS NULL
+AND  CM.inactive_date IS NULL
+
 GROUP BY CMU.clients_music_id
