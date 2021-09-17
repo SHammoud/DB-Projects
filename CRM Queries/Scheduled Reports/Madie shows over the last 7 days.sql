0e1094da3caa752afe470a050ee39f4a3716b5f0
Sam Hammoud <sam@hammoud.co.uk>
2021-09-17 19:14:09 +0100

    Update Madie 7 day report

--- CRM Queries/Scheduled Reports/Madie shows over the last 7 days.sql
+++ CRM Queries/Scheduled Reports/Madie shows over the last 7 days.sql
@@ -21,16 +21,18 @@ SELECT
 #        C.country                                                                  AS 'Country',
 #        T.name                                                                     AS 'Territory',
        CX.code                                                                    AS 'Currency',
-       CASE WHEN DD.cancelled + D.cancelled > 0 THEN COALESCE(DDCF.fee, DCF.fee, '0')
+       CASE WHEN DD.cancelled + D.cancelled > 0 THEN ROUND(COALESCE(DDCF.fee, DCF.fee, NULL),2)
             ELSE DD.fee + COALESCE(ROUND(OV.amount / 100, 2),'0')
 
        END                                                                        AS 'Fee',
        COALESCE(DD.commissionRate, A.commissionRate) / 100                        AS 'Commission Rate',
        COALESCE(DD.exchangeRate,CX.rate)                                               AS 'Exchange Rate',
-       CASE WHEN DD.cancelled + D.cancelled > 0 THEN ROUND((COALESCE(DDCF.fee, DCF.fee,'0') + COALESCE((OV.amount / 100),0)) / COALESCE(DD.exchangeRate, CX.rate)      ,2)
+       CASE WHEN DD.cancelled + D.cancelled > 0 THEN ROUND((COALESCE(DDCF.fee, DCF.fee,'0'))  / COALESCE(DD.exchangeRate, CX.rate)      ,2)
             ELSE ROUND((COALESCE(DD.fee) + COALESCE((OV.amount / 100),0)) / COALESCE(DD.exchangeRate, CX.rate)      ,2)
        END AS 'Total Fee GBP',
-       ROUND((((COALESCE(DDCF.fee, DCF.fee, DD.fee) + COALESCE((OV.amount / 100),0)) * COALESCE(DD.commissionRate, A.commissionRate) / 100)) / COALESCE(DD.exchangeRate, CX.rate)      ,2) AS 'Total Commision GBP'
+       CASE WHEN DD.cancelled + D.cancelled > 0 THEN ROUND((((COALESCE(DDCF.fee, DCF.fee,'0')) * COALESCE(DD.commissionRate, A.commissionRate) / 100)) / COALESCE(DD.exchangeRate, CX.rate)      ,2)
+           ELSE ROUND((((COALESCE(DD.fee) + COALESCE((OV.amount / 100),0)) * COALESCE(DD.commissionRate, A.commissionRate) / 100)) / COALESCE(DD.exchangeRate, CX.rate)      ,2)
+          END AS 'Total Commision GBP'
 
 FROM Deal_Date DD
          LEFT JOIN Deal D ON D.id = DD.dealID
@@ -50,5 +52,5 @@ FROM Deal_Date DD
                     GROUP BY showID) OV ON OV.showID = DD.id
 WHERE DD.date BETWEEN DATE_SUB(NOW(), INTERVAL 12 DAY) AND DATE_SUB(NOW(), INTERVAL 5 DAY)
 AND DD.dealID IS NOT NULL
-HAVING Fee > 0
+HAVING Fee IS NOT NULL
 ORDER BY DD.date
